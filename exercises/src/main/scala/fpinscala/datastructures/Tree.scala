3f94055a282c7cc5348df99f11c6d73328ea4141
Krzysztof Palka <palkakrzysiek@gmail.com>
2018-10-01 09:06:39 +0200

    Ex 3.25 - 3.29

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -11,5 +11,32 @@ object Tree {
     case Branch(l, r) => size(l) + size(r)
   }
 
+  def maximum(t: Tree[Int]): Int = t match {
+    case Leaf(v) => v
+    case Branch(l, r) => maximum(l) max maximum(r)
+  }
+
+  def depth[A](t: Tree[A], d: Int = 0): Int = t match {
+    case Leaf(_) => d + 1
+    case Branch(l, r) => depth(l, d + 1) max depth(r, d + 1)
+  }
+
+  def map[A, B](t: Tree[A])(f: A => B): Tree[B] = t match {
+    case Leaf(v) => Leaf(f(v))
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+  }
+
+  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = t match {
+    case Leaf(v) => f(v)
+    case Branch(l, r) => g(fold(l)(f)(g), fold(r)(f)(g))
+  }
+
+  def sizeUsingFold[A](t: Tree[A]): Int = fold(t)(_ => 1)(_ + _)
+
+  def maximumUsingFold(t: Tree[Int]): Int = fold(t)(identity)(_ max _)
+
+  def depthUsingFold[A](t: Tree[A]): Int = fold(t)(_ => 1)((a, b) => 1 + (a max b))
+
+  def mapUsingFold[A, B](t: Tree[A])(f: A => B): Tree[B] = fold(t)(l => Leaf(f(l)): Tree[B])((l, r) => Branch(l, r))
 
 }
--- exercises/src/main/scala/fpinscala/datastructures/TreeTest.scala
+++ exercises/src/main/scala/fpinscala/datastructures/TreeTest.scala
@@ -15,4 +15,81 @@ class TreeTest extends FlatSpec with Matchers {
     }
   }
 
+  for ((l, m) <- Seq(
+    (Leaf(0), 0),
+    (Branch(Leaf(10), Leaf(0)), 10),
+    (Branch(Leaf(0), Leaf(10)), 10),
+    (Branch(Branch(Leaf(0), Leaf(0)), Leaf(10)), 10)
+  )) {
+    "Tree.maximum" should s"return $m for $l" in {
+      Tree.maximum(l) should be (m)
+    }
+  }
+
+  for ((l, d) <- Seq(
+    (Leaf(0), 1),
+    (Branch(Leaf(10), Leaf(0)), 2),
+    (Branch(Branch(Leaf(0), Leaf(0)), Leaf(10)), 3),
+    (Branch(Leaf(10), Branch(Leaf(0), Leaf(0))), 3)
+  )) {
+    "Tree.depth" should s"return $d for $l" in {
+      Tree.depth(l) should be (d)
+    }
+  }
+
+  for ((l, m) <- Seq(
+    (Leaf(0), Leaf(1)),
+    (Branch(Leaf(10), Leaf(0)), Branch(Leaf(11), Leaf(1))),
+    (Branch(Branch(Leaf(0), Leaf(0)), Leaf(10)), Branch(Branch(Leaf(1), Leaf(1)), Leaf(11))),
+    (Branch(Leaf(10), Branch(Leaf(0), Leaf(0))), Branch(Leaf(11), Branch(Leaf(1), Leaf(1))))
+  )) {
+    "Tree.map" should s"return $m for $l by applying x => x + 1 on each element fot the tree" in {
+      Tree.map(l)(_ + 1) should be (m)
+    }
+  }
+
+  for ((l, s) <- Seq(
+    (Leaf(""), 1),
+    (Branch(Leaf(""), Leaf("")), 2),
+    (Branch(Branch(Leaf(""), Leaf("")), Leaf("")), 3),
+    (Branch(Leaf(""), Branch(Leaf(""), Leaf(""))), 3)
+  )) {
+    "Tree.sizeUsingFold" should s"return $s for $l" in {
+      Tree.sizeUsingFold(l) should be (s)
+    }
+  }
+
+  for ((l, m) <- Seq(
+    (Leaf(0), 0),
+    (Branch(Leaf(10), Leaf(0)), 10),
+    (Branch(Leaf(0), Leaf(10)), 10),
+    (Branch(Branch(Leaf(0), Leaf(0)), Leaf(10)), 10)
+  )) {
+    "Tree.maximumUsingFold" should s"return $m for $l" in {
+      Tree.maximumUsingFold(l) should be (m)
+    }
+  }
+
+  for ((l, d) <- Seq(
+    (Leaf(0), 1),
+    (Branch(Leaf(10), Leaf(0)), 2),
+    (Branch(Branch(Leaf(0), Leaf(0)), Leaf(10)), 3),
+    (Branch(Leaf(10), Branch(Leaf(0), Leaf(0))), 3)
+  )) {
+    "Tree.depthUsingFold" should s"return $d for $l" in {
+      Tree.depthUsingFold(l) should be (d)
+    }
+  }
+
+  for ((l, m) <- Seq(
+    (Leaf(0), Leaf(1)),
+    (Branch(Leaf(10), Leaf(0)), Branch(Leaf(11), Leaf(1))),
+    (Branch(Branch(Leaf(0), Leaf(0)), Leaf(10)), Branch(Branch(Leaf(1), Leaf(1)), Leaf(11))),
+    (Branch(Leaf(10), Branch(Leaf(0), Leaf(0))), Branch(Leaf(11), Branch(Leaf(1), Leaf(1))))
+  )) {
+    "Tree.mapUsingFold" should s"return $m for $l by applying x => x + 1 on each element fot the tree" in {
+      Tree.mapUsingFold(l)(_ + 1) should be (m)
+    }
+  }
+
 }
