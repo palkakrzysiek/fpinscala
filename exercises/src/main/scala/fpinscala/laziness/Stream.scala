1f5cd3aa1ed7066837737cf393885447c26ee365
Krzysztof Palka <k.palka@oberthur.com>
2018-10-06 10:45:46 +0000

    Ex 5.2 - 5.7

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -1,15 +1,16 @@
 package fpinscala.laziness
 
 import Stream._
+
 trait Stream[+A] {
 
   def foldRight[B](z: => B)(f: (A, => B) => B): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
     this match {
-      case Cons(h,t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
+      case Cons(h, t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
       case _ => z
     }
 
-  def exists(p: A => Boolean): Boolean = 
+  def exists(p: A => Boolean): Boolean =
     foldRight(false)((a, b) => p(a) || b) // Here `b` is the unevaluated recursive step that folds the tail of the stream. If `p(a)` returns `true`, `b` will never be evaluated and the computation terminates early.
 
   @annotation.tailrec
@@ -17,22 +18,82 @@ trait Stream[+A] {
     case Empty => None
     case Cons(h, t) => if (f(h())) Some(h()) else t().find(f)
   }
-  def take(n: Int): Stream[A] = ???
 
-  def drop(n: Int): Stream[A] = ???
+  def reverse: Stream[A] = {
+    def loop(acc: Stream[A], rest: Stream[A]): Stream[A] = rest match {
+      case Empty => acc
+      case Cons(h, t) => loop(Cons(h, () => acc), t())
+    }
 
-  def takeWhile(p: A => Boolean): Stream[A] = ???
+    loop(empty, this)
+  }
 
-  def forAll(p: A => Boolean): Boolean = ???
+  def take(n: Int): Stream[A] = {
+    def loop(acc: Stream[A], rest: Stream[A], nn: Int): Stream[A] = (rest,
+      nn) match {
+      case (Empty, _) => acc.reverse
+      case (_, 0) => acc.reverse
+      case (Cons(h, t), _) => loop(Cons(h, () => acc), t(), nn - 1)
+    }
 
-  def headOption: Option[A] = ???
+    loop(empty, this, n)
+  }
+
+  def drop(n: Int): Stream[A] = {
+    def loop(rest: Stream[A], nn: Int): Stream[A] = if (nn <= 0) rest else rest
+    match {
+      case Empty => Empty
+      case Cons(_, t) => loop(t(), nn - 1)
+    }
+
+    loop(this, n)
+  }
+
+  def takeWhile(p: A => Boolean): Stream[A] = {
+    def loop(acc: Stream[A], rest: Stream[A]): Stream[A] = rest match {
+      case Empty => acc.reverse
+      case Cons(h, _) if !p(h()) => acc.reverse
+      case Cons(h, t) => loop(Cons(h, () => acc), t())
+    }
+
+    loop(empty, this)
+  }
+
+  def forAll(p: A => Boolean): Boolean = this match {
+    case Empty => true
+    case Cons(h, t) => p(h()) && t().forAll(p)
+  }
+
+  def takeWhileViaFoldRight(p: A => Boolean): Stream[A] = this
+    .foldRight(empty: Stream[A]) { (e, acc) =>
+      if (p(e)) cons(e, acc)
+      else empty
+    }
+
+  def headOption: Option[A] = this.foldRight(None: Option[A])((h, _) => Some(h))
 
   // 5.7 map, filter, append, flatmap using foldRight. Part of the exercise is
   // writing your own function signatures.
 
+  def map[B](f: A => B): Stream[B] = this
+    .foldRight(empty: Stream[B])((a, b) => cons(f(a), b))
+
+  def filter(p: A => Boolean): Stream[A] = this
+    .foldRight(empty[A])((a, b) => if (p(a)) cons(a, b) else b)
+
+  def append[B >: A](s: => Stream[B]): Stream[B] = this
+    .foldRight(s)((a, b) => cons(a, b))
+
+  def flatMap[B](f: A => Stream[B]): Stream[B] = this
+    .foldRight(empty[B])((a, b) => f(a) append b)
+
   def startsWith[B](s: Stream[B]): Boolean = ???
+
+  def toList: List[A] = this.foldRight(Nil: List[A])(_ +: _)
 }
+
 case object Empty extends Stream[Nothing]
+
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
 
 object Stream {
@@ -45,10 +106,11 @@ object Stream {
   def empty[A]: Stream[A] = Empty
 
   def apply[A](as: A*): Stream[A] =
-    if (as.isEmpty) empty 
+    if (as.isEmpty) empty
     else cons(as.head, apply(as.tail: _*))
 
   val ones: Stream[Int] = Stream.cons(1, ones)
+
   def from(n: Int): Stream[Int] = ???
 
   def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = ???
---
+++ exercises/src/main/scala/fpinscala/laziness/StreamTest.scala
@@ -0,0 +1,143 @@
+package fpinscala.laziness
+
+import org.scalatest.FunSuite
+
+class StreamTest extends FunSuite {
+
+  import Stream._
+
+  test("Stream.toList") {
+    assert(Stream(1, 2, 3).toList === List(1, 2, 3))
+  }
+
+  test("Stream.take") {
+    var zero = 0
+
+    def effectfulZero = {
+      zero = 1
+      0
+    }
+
+    assert(cons(2, cons(1, cons(effectfulZero, empty))).take(2).toList ===
+      List(2, 1), "takes only first n parameters")
+    assert(zero === 0, "don't force thunk")
+  }
+
+  test("Stream.drop") {
+    var zero = 0
+
+    def effectfulTwo = {
+      zero = 1
+      2
+    }
+
+    assert(cons(effectfulTwo, cons(1, cons(0, empty))).drop(2).toList ===
+      List(0), "drops n first parameters")
+    assert(zero === 0, "don't force thunk")
+  }
+
+  test("Stream.takeWhile") {
+
+    var zero = 0
+
+    def effectfulZero = {
+      zero = 1
+      0
+    }
+
+    assert(cons(2, cons(1, cons(effectfulZero, empty))).takeWhile(_ > 1).toList
+      ===
+      List(2), "takes only first n parameters")
+    assert(zero === 0, "don't force thunk")
+  }
+
+  test("Stream.forAll") {
+
+    var zero = 0
+
+    def effectfulZero = {
+      zero = 1
+      0
+    }
+
+    assert(cons(2, cons(1, cons(effectfulZero, empty)))
+      .forAll(_ > 1) === false, "takes only first n parameters")
+    assert(zero === 0, "don't force thunk")
+    assert(Stream(1, 2, 3).forAll(_ > 0) === true)
+  }
+
+  test("Stream.takeWhileViaFoldRight") {
+
+    var zero = 0
+
+    def effectfulZero = {
+      zero = 1
+      0
+    }
+
+    assert(cons(2, cons(1, cons(effectfulZero, empty)))
+      .takeWhileViaFoldRight(_ > 1).toList === List(2),
+      "takes only first n parameters")
+    assert(zero === 0, "don't force thunk")
+  }
+
+  test("Stream.headOption") {
+
+    var zero = 0
+
+    def effectfulZero = {
+      zero = 1
+      0
+    }
+
+    assert(cons(2, cons(1, cons(effectfulZero, empty)))
+      .headOption === Some(2),
+      "takes only first n parameters")
+    assert(zero === 0, "don't force thunk")
+    assert(empty.headOption.isEmpty)
+  }
+
+  test("Stream.map") {
+
+    assert(Stream(1, 2, 3).map(_ + 1).toList === List(2, 3, 4))
+
+    var zero = 0
+
+    def effectfulZero = {
+      zero = 1
+      0
+    }
+
+    assert(cons(2, cons(1, cons(effectfulZero, empty)))
+      .map(_ + 1)
+      .headOption === Some(3))
+    assert(zero === 0, "don't force thunk")
+    assert(empty.headOption.isEmpty)
+  }
+
+  test("Stream.filter") {
+
+    var zero = 0
+
+    def effectfulZero = {
+      zero = 1
+      0
+    }
+
+    assert(cons(2, cons(1, cons(effectfulZero, empty)))
+      .filter(_ % 2 == 1)
+      .headOption === Some(1))
+    assert(zero === 0, "don't force thunk")
+    assert(empty.headOption.isEmpty)
+  }
+
+  test("Stream.append") {
+    assert(Stream(1).append(Stream("A")).toList === List(1, "A"))
+  }
+
+  test("Stream.flatMap") {
+    assert(Stream(1, 2)
+      .flatMap(a => Stream(a.toString)).toList === List("1", "2"))
+  }
+
+}
