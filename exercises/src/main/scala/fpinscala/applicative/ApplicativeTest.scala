bb1e2dad8ceeae1da3fe7bec313274851f8e6cb8
Krzysztof Palka <palkakrzysiek@gmail.com>
2018-12-01 09:28:49 +0100

    Ex 12.4

---
+++ exercises/src/main/scala/fpinscala/applicative/ApplicativeTest.scala
@@ -0,0 +1,21 @@
+package fpinscala.applicative
+
+import org.scalatest.{FunSuite, Matchers}
+
+import scala.collection.immutable
+
+class ApplicativeTest extends FunSuite with Matchers {
+  test("stream applicative") {
+    import Applicative._
+    val sequenced: Stream[List[Int]] = streamApplicative
+      .sequence(List(
+        Stream(1, 2, 3),
+        Stream(4, 5, 6),
+        Stream.continually(0)
+      ))
+    sequenced(0) should be(List(1, 4, 0))
+    sequenced(1) should be(List(2, 5, 0))
+    sequenced(2) should be(List(3, 6, 0))
+    an [IndexOutOfBoundsException] should be thrownBy sequenced(3)
+  }
+}
