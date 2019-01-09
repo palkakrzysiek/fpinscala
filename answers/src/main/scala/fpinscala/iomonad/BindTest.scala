0a3d6ebcd0ea6f31478868e011b102430078015e
Krzysztof Palka <palkakrzysiek@gmail.com>
2019-01-09 09:02:59 +0100

    play with io monad

--- answers/src/main/scala/fpinscala/iomonad/BindTest.scala
+++ answers/src/main/scala/fpinscala/iomonad/BindTest.scala
@@ -5,11 +5,11 @@ import language.postfixOps
 
 object BindTest extends App {
 
-  def timeit(n: Int)(task: => Unit): Unit = {
+  def timeit(n: Int, tag: String)(task: => Unit): Unit = {
     val start = System.currentTimeMillis
     (0 to n).foreach { _ => task }
     val stop = System.currentTimeMillis
-    println((stop - start) / 1000.0 + " seconds")
+    println(tag + ": " + (stop - start) / 1000.0 + " seconds")
   }
 
   val N = 100000
@@ -32,16 +32,32 @@ object BindTest extends App {
     def flatMap[A,B](pa: Par[A])(f: A => Par[B]) = Par.fork { Par.flatMap(pa)(f) }
   }
 
-  val pool = java.util.concurrent.Executors.newFixedThreadPool(4)
+  def repeat[A](n: Int)(v: => A) = (0 to n).map(_ => v)
 
-  timeit(10) { go(Throw)(Throw.unit(())) ( _ run ) }
-  timeit(10) { go(IO2b.TailRec)(IO2b.TailRec.unit(())) ( IO2b.run ) }
-  timeit(10) { go(IO2c.Async)(IO2c.Async.unit(()))(r => Par.run(pool) { IO2c.run(r) }) }
-  timeit(10) { go[IO](ioMonad)(ioMonad.unit(()))(r => unsafePerformIO(r)(pool)) }
-  timeit(10) { go(Task)(Task.now(()))(r => r.run(pool)) }
-  timeit(10) { go(Task)(Task.forkUnit(()))(r => r.run(pool)) }
-  timeit(10) { go(ParMonad)(ParMonad.unit(())) { p => Par.run(pool)(p) }}
+  for (nThreads <- 1 until 4) {
+    println(s"== Using $nThreads threads:")
+    val pool = java.util.concurrent.Executors.newFixedThreadPool(nThreads)
+  repeat(5)(timeit(10, "Throw") { go(Throw)(Throw.unit(())) ( _ run ) })
+    repeat(5)(timeit(10, "TailRec") { go(IO2b.TailRec)(IO2b.TailRec.unit(())) ( IO2b.run ) })
+    repeat(5)(timeit(10, "Async") { go(IO2c.Async)(IO2c.Async.unit(()))(r => Par.run(pool) { IO2c.run(r) }) })
+    repeat(5)(timeit(10, "ioMonad") { go[IO](ioMonad)(ioMonad.unit(()))(r => unsafePerformIO(r)(pool)) })
+    repeat(5)(timeit(10, "Task#now") { go(Task)(Task.now(()))(r => r.run(pool)) })
+    repeat(5)(timeit(10, "Task#forkUnit") { go(Task)(Task.forkUnit(()))(r => r.run(pool)) })
+    repeat(5)(timeit(10, "ParMonad") { go(ParMonad)(ParMonad.unit(())) { p => Par.run(pool)(p) }})
+    pool.shutdown()
+  }
+
+
+
+  /**
+    * Throw: 0.795 seconds
+    * TailRec: 0.861 seconds
+    * Async: 0.654 seconds
+    * ioMonad: 0.415 seconds
+    * Task#now: 1.57 seconds
+    * Task#forkUnit: 2.247 seconds
+    * ParMonad: 1.461 seconds
+    */
 
   // Par.run(pool)(ParMonad.forever { ParMonad.unit { println("woot") }})
-  pool.shutdown()
 }
